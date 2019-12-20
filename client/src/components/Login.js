037575b21967d2a306462728b3d2bc69e7689c47
wchamber01 <williamchambers@gmail.com>
2019-12-20 11:35:16 -0500

    setup state and handlechange

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axiosWithAuth from "./utils/axiosWithAuth";
 
-const Login = () => {
+const Login = props => {
   // make a post request to retrieve a token from the api
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: ""
+  });
+
+  const handleChange = e =>
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  console.log(credentials, "handleChange");
+
   // when you have handled the token, navigate to the BubblePage route
   return (
     <>
